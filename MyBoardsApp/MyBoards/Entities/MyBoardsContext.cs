dc87192666c87b15427af2c0bf721c94dbf834de
Jan Szczepan <janszczepan62@gmail.com>
2024-01-15 21:38:44 +0100

    Model configuration

--- MyBoardsApp/MyBoards/Entities/MyBoardsContext.cs
+++ MyBoardsApp/MyBoards/Entities/MyBoardsContext.cs
@@ -9,4 +9,17 @@ public class MyBoardsContext(DbContextOptions<MyBoardsContext> options) : DbCont
     public DbSet<Tag> Tags { get; set; }
     public DbSet<Comment> Comments { get; set; }
     public DbSet<Address> Addresses { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<WorkItem>(eb =>
+        {
+            eb.Property(wi => wi.State).IsRequired();
+            eb.Property(wi => wi.Area).HasMaxLength(200);
+            eb.Property(wi => wi.Effort).HasPrecision(5, 2);
+            eb.Property(wi => wi.EndDate).HasPrecision(3);
+            eb.Property(wi => wi.Activity).HasMaxLength(200);
+            eb.Property(wi => wi.RemainingWork).HasPrecision(14, 2);
+        });
+    }
 }
